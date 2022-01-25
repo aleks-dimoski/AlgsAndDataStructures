e6c362ec7485d0063f7ce393d114e4d80d3b65fa
aleks-dimoski <aleksdimoski890@gmail.com>
2022-01-25 14:00:49 -0500

    Added Breadth First Search and prototype DFS

--- Graph Theory/Basic/DFS/DFS.cc
+++ Graph Theory/Basic/DFS/DFS.cc
@@ -1,80 +1,145 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
 #include <vector>
-#include <stack>
 #include <algorithm>
 
 using namespace std;
 
-bool increasing(uint32_t a, uint32_t b) { return a<b; }
-
-class CSRGraph {
+// LinkedList class to act as helper, and as data structure used to hold adjacencies. Nodes are labeled as integer values
+class LinkedList {
 private:
-  uint32_t numNodes;
-  vector<uint32_t> start;
-  vector<uint32_t> adjacency;
-  vector<double> w;
-
+  uint32_t size;
+  class Node {
+  public:
+    int32_t val;
+    double weight;
+    Node* next;
+    Node(int32_t val=0, double weight=0., Node* next=nullptr) : val(val), next(next), weight(weight) {}
+    ~Node() { delete next; }
+  };
+  Node* head;
 public:
-  CSRGraph(uint32_t numNodes, uint32_t numCon) : numNodes(numNodes) {
-    for(uint32_t i = 0; i < numNodes; i++)
-      start.push_back(i);
-    for(uint32_t i = 0; i < numCon-numNodes; i++)
-      start.push_back(1+rand()%numNodes);
-    sort(start.begin(), start.end(), increasing);
-    for(uint32_t i = 0; i < numCon; i++) {
-      uint32_t newAdjacency = 0;
-      do {
-        newAdjacency = 1+rand()%numNodes;
-      } while(newAdjacency == start[i]);
-      adjacency.push_back(newAdjacency);
-      w.push_back(1.*(rand()%1000)/1000);
-    }
+  LinkedList() : head(nullptr), size(0) {}
+  void addStart(int32_t val, double weight) {
+    size++;
+    Node* a = new Node(val, weight, head);
+    head = a;
   }
-  
-  friend ostream& operator <<(ostream& s, const CSRGraph& g) {
-    for(uint32_t i = 0; i < g.start.size(); i++) {
-      s << g.start[i] << '\t' << g.adjacency[i] << '\t' << g.w[i] << '\n';
+  void addEnd(int32_t val, double weight) {
+    size++;
+    if(head == nullptr) {
+      addStart(val, weight);
+    }
+    Node* p = head;
+    while(p->next != nullptr) {
+      p = p->next;
+    }
+    p->next = new Node(val, weight, nullptr);
+  }
+  int32_t removeStart() {
+    int32_t hold = -1;
+    if(head == nullptr) {
+      std::cout << "\nError. LinkedList is empty.\n";
+      return -1;
+    } else if(head->next == nullptr) {
+      hold = head->val;
+      head = nullptr;
+    } else {
+      hold = head->val;
+      Node* n = head->next;
+      head->next = head->next->next;
+      head = n;
+    }
+    size--;
+    return hold;
+  }
+  int32_t removeEnd() {
+    Node* p = head;
+    int32_t hold;
+    if(head == nullptr) {
+      std::cout << "\nError. LinkedList is empty.\n";
+      return -1;
+    }
+    if(head->next == nullptr) { return removeStart(); }
+    while(p->next->next != nullptr) {
+      hold = p->val;
+      p = p->next;
+    }
+    hold = p->next->val;
+    p->next = nullptr;
+    size--;
+    return hold;
+  }
+  bool contains(uint32_t v) {
+    if(head == nullptr) { return false; }
+    Node* p = head;
+    while(p->next != nullptr) {
+      if(p->val == v) { return true; }
+      p = p->next;
+    }
+    if(p->val == v) { return true; }
+    return false;
+  }
+  uint32_t len() const { return size; }
+  friend ostream& operator<<(ostream& s, const LinkedList& l) {
+    Node* p = l.head;
+    while(p != nullptr) {
+      s << p->val << " " << p->weight << "  ";
+      p = p->next;
     }
     return s;
   }
   
-  vector<uint32_t> adjacent(uint32_t v) {
-    vector<uint32_t> ad;
-    for(uint32_t i = v; start[i] <= v && i < adjacency.size(); i++)
-      if(start[i] == v) { ad.push_back(adjacency[i]); }
-    return ad;
-  }
+};
+
+
+// Beginning of actual adjacency list class
+class adjacencyList {
+private:
+  vector<LinkedList*> start;  
   
-  void dfs(uint32_t v) {
-    vector<bool> scheduled;
+public:
+  adjacencyList(uint32_t numNodes) {
+    // Generates a new fully-connected graph
     for(uint32_t i = 0; i < numNodes; i++)
-      scheduled.push_back(false);
-    stack<uint32_t> nodes;
-    nodes.push(v);
-    scheduled[v] = true;
-    while(!nodes.empty()) {
-      v = nodes.top();
-      nodes.pop();
-      cout << v << '\n';
-      vector<uint32_t> adj = adjacent(v);
-      for(uint32_t v2 : adj) {
-        cout << v2 << '\t';
-        if(!scheduled[v2]) {
-          nodes.push(v2);
-          scheduled[v2] = true;
-        }
-        cout << '\n';
+      start.push_back(new LinkedList());
+    for(uint32_t i = 0; i < uint32_t(numNodes*1.3); i++) {
+      uint32_t ind1 = rand()%numNodes;
+      uint32_t ind2 = rand()%numNodes;
+      if(ind1 != ind2 && !start[ind1]->contains(ind2) && !start[ind2]->contains(ind1)) {
+        double weight = (rand()%1000)/1000.;
+        connect(ind1, ind2, weight);
       }
     }
+    for(uint32_t i = 0; i < numNodes; i++) {
+      if(start[i]->len() == 0) {
+        uint32_t ind;
+        do {
+          ind = rand()%numNodes;
+        } while(ind == i);
+        double weight = (rand()%1000)/1000.;
+        connect(i, ind, weight);
+      }
+    }
+  }
+
+  void connect(uint32_t ind1, uint32_t ind2, double weight) {
+    start[ind1]->addStart(ind2, weight);
+    start[ind2]->addStart(ind1, weight);
+  }
+
+  friend ostream& operator <<(ostream& s, const adjacencyList& a) {
+    for(uint32_t i = 0; i < a.start.size(); i++) {
+      s << i << "\t" ;
+      s << *(a.start[i]) << '\n';
+    }
+    return s;
   }
   
 };
 
 int main() {
-  CSRGraph g(5, 8);
-  cout << g;
-  g.dfs(0);
-  
+  adjacencyList g(15);
+  cout << g << '\n';
 }
+
